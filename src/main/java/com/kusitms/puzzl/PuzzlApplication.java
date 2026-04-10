675b166bcba9770cb36bc720de81f0f4adbd4aa1
kyjin <kyjin1333@gmail.com>
2026-04-11 01:08:52 +0900

    init : project setup

---
+++ src/main/java/com/kusitms/puzzl/PuzzlApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.puzzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusitms/puzzl/PuzzlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.puzzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
