47379b212a2c8cb86240168b12c1162084054041
erika0915 <soohee2001@gmail.com>
2026-04-22 22:29:08 +0900

    feat: 스웨거 및 db 관련 설정 (#1)

---
+++ src/main/java/com/kusitms/kkium/global/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.kusitms.kkium.global.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.server.url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme =
+                new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
+        return new OpenAPI()
+                .addServersItem(
+                        new io.swagger.v3.oas.models.servers.Server()
+                                .url(serverUrl)
+                                .description("Auto Configured Server"))
+                .components(new Components().addSecuritySchemes("BearerAuth", securityScheme))
+                .info(
+                        new Info()
+                                .title("끼움 REST API")
+                                .description("Kkium Backend Team")
+                                .contact(
+                                        new Contact().name("Kkium BE Github").url("https://github.com/team-KKIUM/KKIUM-BE"))
+                                .version("1.0.0"))
+                .addSecurityItem(securityRequirement);
+    }
+}
