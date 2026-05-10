c0347beccbdaf1e059f551d183c8661d5de3859e
jiwonniy <jiwoni0707@gmail.com>
2026-05-10 18:46:43 +0900

    chore: 코드 포맷팅 적용 (#15)

--- src/main/java/com/kusitms/kkium/global/controller/HealthController.java
+++ src/main/java/com/kusitms/kkium/global/controller/HealthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
