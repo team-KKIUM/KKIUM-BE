1f6a6814b3f2a3f94eea98587b5763d42cc3f146
jiwonniy <jiwoni0707@gmail.com>
2026-05-10 18:41:24 +0900

    chore: import 순서 변경 (#15)

--- src/main/java/com/kusitms/kkium/global/controller/HealthController.java
+++ src/main/java/com/kusitms/kkium/global/controller/HealthController.java
@@ -1,11 +1,11 @@
 package com.kusitms.kkium.global.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Health", description = "서버 상태 확인 API")
 @RestController
