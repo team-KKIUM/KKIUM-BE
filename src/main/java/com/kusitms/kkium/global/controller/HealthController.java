ccab3686a26ba833620e929e8c30f0b5adac225b
jiwonniy <jiwoni0707@gmail.com>
2026-05-10 18:26:47 +0900

    chore: swagger 설명 추가 (#15)

--- src/main/java/com/kusitms/kkium/global/controller/HealthController.java
+++ src/main/java/com/kusitms/kkium/global/controller/HealthController.java
@@ -1,14 +1,18 @@
 package com.kusitms.kkium.global.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Health", description = "서버 상태 확인 API")
 @RestController
 @RequestMapping("/api/v1")
 public class HealthController {
 
+  @Operation(summary = "헬스 체크", description = "서버가 정상적으로 동작하는지 확인합니다.")
   @GetMapping("/health")
   public ResponseEntity<String> health() {
     return ResponseEntity.ok("OK");
