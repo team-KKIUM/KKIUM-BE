dd3f39f51dafff5cc91d27d7a71c159631cd1474
jiwonniy <jiwoni0707@gmail.com>
2026-05-15 00:46:47 +0900

    style: 스웨거 주석 수정 (#20)

--- src/main/java/com/kusitms/kkium/jd/controller/JdController.java
+++ src/main/java/com/kusitms/kkium/jd/controller/JdController.java
@@ -94,8 +94,7 @@ public class JdController {
 
   @Operation(
       summary = "[지원 관리] 공고 전체 목록 조회",
-      description =
-          "로그인한 사용자의 지원 관리에서 공고 목록을 페이지네이션으로 조회합니다. keyword 입력 시 공고명/기업명/모집분야 기준으로 필터링합니다.")
+      description = "로그인한 사용자의 지원 관리에서 공고 목록을 조회합니다. keyword 입력 시 공고명/기업명/모집분야에 포함된 단어 기준으로 검색합니다.")
   @GetMapping
   public ResponseEntity<ApiResponse<JdListPageResponse>> getJdList(
       @AuthenticationPrincipal CustomUserDetails userDetails,
