b16c09c932e8a635b520716cedbf1159fc788a83
erika0915 <soohee2001@gmail.com>
2026-05-15 13:20:40 +0900

    feat: experience 도메인 엔티티 및 레포지토리 추가 (#42)

---
+++ src/main/java/com/kusitms/kkium/experience/repository/CareerRepository.java
@@ -0,0 +1,7 @@
+package com.kusitms.kkium.experience.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kusitms.kkium.experience.domain.Career;
+
+public interface CareerRepository extends JpaRepository<Career, Long> {}
---
+++ src/main/java/com/kusitms/kkium/experience/repository/PieceRepository.java
@@ -0,0 +1,11 @@
+package com.kusitms.kkium.experience.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kusitms.kkium.experience.domain.Piece;
+
+public interface PieceRepository extends JpaRepository<Piece, Long> {
+  List<Piece> findByUserId(Long userId);
+}
