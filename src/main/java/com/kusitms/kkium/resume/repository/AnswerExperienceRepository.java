44afeb4a677d81681af31c70e0e5e5ad4f50386b
kyjin <kyjin320@sookmyung.ac.kr>
2026-05-21 21:11:55 +0900

    refactor: AnswerExperience 벌크 삭제 쿼리 적용

--- src/main/java/com/kusitms/kkium/resume/repository/AnswerExperienceRepository.java
+++ src/main/java/com/kusitms/kkium/resume/repository/AnswerExperienceRepository.java
@@ -3,6 +3,9 @@ package com.kusitms.kkium.resume.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kusitms.kkium.resume.domain.AnswerExperience;
 
@@ -10,5 +13,7 @@ public interface AnswerExperienceRepository extends JpaRepository<AnswerExperien
 
   List<AnswerExperience> findByJdAnswerId(Long jdAnswerId);
 
-  void deleteByJdAnswerId(Long jdAnswerId);
+  @Modifying
+  @Query("DELETE FROM AnswerExperience ae WHERE ae.jdAnswer.id IN :answerIds")
+  void deleteAllByJdAnswerIdIn(@Param("answerIds") List<Long> answerIds);
 }
