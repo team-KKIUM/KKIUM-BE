639c5e846e715777501d8e218919f75ec42ae72a
jiwonniy <jiwoni0707@gmail.com>
2026-05-29 17:12:05 +0900

    fix: 경험 제목 수정 시 글자 수 제한 추가 (#113)

--- src/main/java/com/kusitms/kkium/experience/dto/request/ExperienceTitleUpdateRequest.java
+++ src/main/java/com/kusitms/kkium/experience/dto/request/ExperienceTitleUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.kusitms.kkium.experience.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record ExperienceTitleUpdateRequest(@NotBlank(message = "제목을 입력해주세요.") String title) {}
+public record ExperienceTitleUpdateRequest(
+    @NotBlank(message = "제목을 입력해주세요.") @Size(max = 80, message = "제목은 80자 이하여야 합니다.")
+        String title) {}
